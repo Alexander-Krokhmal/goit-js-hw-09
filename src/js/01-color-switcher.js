28f7df84e287f56c18f4fcb675ce70c30fd05086
Alexander-Krokhmal <zeddicus.ak@gmail.com>
2022-09-11 08:54:27 +0300

    Add change 1 option

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,46 @@
-refs = {
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+
+// console.log(startBtn);
+startBtn.addEventListener('click', onChangeColor);
+stopBtn.addEventListener('click', onChangeColorStop);
+
+let timerId = null;
+stopBtn.disabled = true;
+
+function onChangeColor () {
+    const body = document.body;
+
+    timerId = setInterval(() => { body.style.backgroundColor = getRandomHexColor(); }, 1000);
+
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    };
+
+function onChangeColorStop() {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+}
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+
+
+
+
+
+
+
+
+
+
+  /**
+   refs = {
     startBtn: document.querySelector('[data-start]'),
     stopBtn: document.querySelector('[data-stop]'),
 };
@@ -29,3 +71,4 @@ function onChangeColorStop() {
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
+   */
